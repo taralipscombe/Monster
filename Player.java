0d86262064b9e81f0c317978bc815b4300bcc9d1
taralipscombe <104409751+taralipscombe@users.noreply.github.com>
2022-05-13 09:00:54 +1200

    Create Player.java

---
+++ Player.java
@@ -0,0 +1,86 @@
+import java.util.ArrayList;
+
+public class Player {
+	
+	public String playerName;
+	public String chosenDifficulty;
+	public int gold;
+	public ArrayList<Item> items = new ArrayList<Item>();
+	public ArrayList<Monster> monsterTeam = new ArrayList<Monster>();
+	public int currentDay;
+	public int finishDay;
+	
+
+	public Player(String playerName2, String difficulty, Monster startingMonster, int numDays) {
+		playerName = playerName2;
+		chosenDifficulty = difficulty;
+		monsterTeam.add(startingMonster);
+		finishDay = numDays;
+		currentDay = 0;
+		setGold();
+	}
+
+	public ArrayList<Item> getItems(){
+		return items;
+	}
+	
+	public void addItem(Item newestItem) {
+		items.add(newestItem);
+	}
+	
+	public void setGold() {
+		if (chosenDifficulty == "Easy") {
+			gold = 200;
+		} else if (chosenDifficulty == "Medium") {
+			gold = 150;
+		} else {
+			gold = 100;
+		}
+	}
+	
+	public int getgold() {
+		return gold;
+	}
+	
+	public ArrayList<Monster> getTeam(){
+		return monsterTeam;
+	}
+	
+	public void addTeamMate(Monster newestMonster) {
+		monsterTeam.add(newestMonster);
+	}
+	
+	// same as sleeping so will need to add how the monsters heal over night
+	public void incrementDay() {
+		currentDay = currentDay + 1;
+		for (Monster monster : monsterTeam) {
+			monster.heal();
+		}
+	}
+	
+	public void endGame() {
+		currentDay = finishDay;
+	}
+	
+	public void printAttributes() {
+		System.out.println("Current gold: "+gold);
+		System.out.println("Current Day: "+currentDay);
+		System.out.println("Days Remaining: "+(finishDay -currentDay));
+		
+	}
+	
+	public void printCurrentTeam() {
+		for(Monster monster : monsterTeam) {
+			System.out.println();
+			monster.commandLineToString();
+		}
+	}
+	
+	public void printInventory() {
+		for(Item item : items) {
+			System.out.println();
+			item.printAttributes();
+		}
+	}
+	
+}
