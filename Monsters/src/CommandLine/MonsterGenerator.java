6becb87be53fe419dd687c4f7468041c2c19aa8a
emmabadger1 <104410096+emmabadger1@users.noreply.github.com>
2022-05-22 13:45:54 +1200

    Update MonsterGenerator.java

--- Monsters/src/CommandLine/MonsterGenerator.java
+++ Monsters/src/CommandLine/MonsterGenerator.java
@@ -3,34 +3,29 @@ import java.util.Arrays;
 import java.util.Random;
 
 /**
- * 
- *MonsterGenerator Class.
- *
+ * The Class MonsterGenerator.
  */
 public class MonsterGenerator {
 	
-	/** Array list of randomly generated monsters*/
+	/** The array list of Monsters to be returned after generated. */
 	private ArrayList<Monster> returnableMonsters = new ArrayList<Monster>();
 	
-	/** Array list of possible names monsters can be generated with*/
+	/** The array list of potential monster names. */
 	private ArrayList<String> monsterNames = new ArrayList<String>(Arrays.asList("Ed", "Bruno", "Micheal", "Justin", "Joe", "Kevin", "Nick", "Billy", "Elton",
 			"Charlie", "Shawn", "John", "Prince"));
 	
+	/**
+	 * Instantiates a new monster generator.
+	 */
 	public MonsterGenerator() {
 	
 	}
 	
-	/**
-	 * gets the monsterNames array list
-	 * @return monsterNames 
-	 */
-	public ArrayList<String> getPossibleNamesList() {
-		return monsterNames;
-	}
 	
 	/**
-	 * generates a random monster 4 times and adds it to the returnableMonsters array list.
-	 * @return the array list after having added the 4 monsters 
+	 * Generator. Generates the array list of Monsters
+	 *
+	 * @return the array list of returnable monsters
 	 */
 	public ArrayList<Monster> generator(){
 		int i = 0;
@@ -43,9 +38,11 @@ public class MonsterGenerator {
 	}
 	
 	
+	
 	/**
-	 * generates a monster with a ranom name, random damage, random heal amount and sets the price to the sum of the damage and heal amounts.
-	 * @return monster
+	 * New monster. Creates a single randomised Monster.
+	 *
+	 * @return the monster created 
 	 */
 	public Monster newMonster() {
 		String name = randomName();
@@ -57,20 +54,42 @@ public class MonsterGenerator {
 	}
 	
 	/**
-	 * Randomly chooses a name from the list monsterNames
-	 * @return a name
+	 * Random name. Selects a random name from the given array list of potential Names
+	 *
+	 * @return the string of the name chosen
 	 */
 	public String randomName() {
 		int range = monsterNames.size();
 	    Random rndm = new Random();
 	    int randomInt = rndm.nextInt(range);
-	    return monsterNames.get(randomInt);
+	    String name = monsterNames.get(randomInt);
+	    while (!checkName(name)) {
+		    int nextRandomInt = rndm.nextInt(range);
+		    name = monsterNames.get(nextRandomInt);
+	    }
+	    return name;
 		
 	}
 	
 	/**
-	 * Randomly chooses an integer in the range (0,50)
-	 * @return integer
+	 * Check name. Checks the name chosen wasnt already a name of another Monster in the returnable team
+	 *
+	 * @param name the name chosen 
+	 * @return true, if successful
+	 */
+	public boolean checkName(String name) {
+		for(Monster monster : returnableMonsters) {
+	    	if (monster.getName().equals(name)) {
+	    		return false;
+	    	}
+	    }
+		return true;
+	}
+	
+	/**
+	 * Random damage. Generates a random damage between 0 and 50 (max Damage initially given to a Monster)
+	 *
+	 * @return the int of the damage given to monster
 	 */
 	public int randomDamage() {
 		Random rndm = new Random();
@@ -79,13 +98,23 @@ public class MonsterGenerator {
 	}
 	
 	/**
-	 * Randomly chooses an integer in the range (0, 35)
-	 * @return integer
+	 * Random heal. Generates a random heal amount between 0 and 35 (max heal amount initially given to a Monster)
+	 *
+	 * @return the int of the heal amount given to a monster
 	 */
 	public int randomHeal() {
 		Random rndm = new Random();
 	    int randomInt = rndm.nextInt(35);
 	    return randomInt;
 	}
-
+	/**
+	 * gets the monsterNames array list
+	 * @return monsterNames 
+	 */
+	public ArrayList<String> getPossibleNamesList() {
+		return monsterNames;
+	}
 }
+
+	
+	
