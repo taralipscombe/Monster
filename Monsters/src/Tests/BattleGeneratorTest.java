41fea9ba1df0704cb31bf32db2cef3a88868f71a
taralipscombe <104409751+taralipscombe@users.noreply.github.com>
2022-05-22 13:11:46 +1200

    Create BattleGeneratorTest.java

---
+++ Monsters/src/Tests/BattleGeneratorTest.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class testBattleGenerator {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
